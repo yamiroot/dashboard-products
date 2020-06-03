125954a5fa4da63bb6224b5865eef2d8a1ada3b3
yamiquispe <yamipqm@gmail.com>
2020-06-03 03:57:44 -0500

    Component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => (
+  <footer role="contentinfo" data-testid="footer">
+    <p className="text-center text-black">
+      <small>Copyleft 🄯  |  Dashboard Products  |  2020</small>
+    </p>
+  </footer>
+);
+
+
+export default Footer;
