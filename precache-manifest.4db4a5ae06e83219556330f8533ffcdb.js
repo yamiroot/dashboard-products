9ad3b2c7c3d545ae4a085c55e01df95e96e19842
yamiquispe <yamipqm@gmail.com>
2020-06-04 16:36:12 -0500

    Updates

---
+++ precache-manifest.4db4a5ae06e83219556330f8533ffcdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0efcae00629aab160e39a7f897ade7b",
+    "url": "/dashboard-products/index.html"
+  },
+  {
+    "revision": "969a58df36e304e6e65b",
+    "url": "/dashboard-products/static/css/main.dcfa2896.chunk.css"
+  },
+  {
+    "revision": "6950d7c8dd33bdf24de2",
+    "url": "/dashboard-products/static/js/2.0d88a22e.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/dashboard-products/static/js/2.0d88a22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969a58df36e304e6e65b",
+    "url": "/dashboard-products/static/js/main.da91503a.chunk.js"
+  },
+  {
+    "revision": "6a0d9e7832a3815455ba",
+    "url": "/dashboard-products/static/js/runtime-main.e38bda1b.js"
+  },
+  {
+    "revision": "875414f95d6f37d3dcf852f10a3477a0",
+    "url": "/dashboard-products/static/media/logotext.875414f9.png"
+  }
+]);
